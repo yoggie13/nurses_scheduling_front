30114da9698266f4e71f00ba04f1a30803d712bb
yoggie13 <ognjennikolic13@gmail.com>
2022-07-05 22:16:09 +0200

    added favicon and animation for notifications

--- src/components/CreateReport.js
+++ src/components/CreateReport.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/styles/CreateReport.css'
 import AutoCompleteComponent from './AutoCompleteComponent';
 import Calendar from './Calendar';
