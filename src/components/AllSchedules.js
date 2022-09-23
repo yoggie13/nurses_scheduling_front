9d80a1ab95d12cf6340c926d66ee1ede99090bbe
Ognjen <ognjennikolic13@gmail.com>
2022-09-24 00:40:48 +0200

    changes for sched bit->tinyint

--- src/components/AllSchedules.js
+++ src/components/AllSchedules.js
@@ -1,89 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
-import '../assets/styles/AllSchedules.css'
-import services from '../services/services';
-import Loading from './Loading';
-import Notification from './Notification';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import "../assets/styles/AllSchedules.css";
+import services from "../services/services";
+import Loading from "./Loading";
+import Notification from "./Notification";
 
 export default function AllSchedules() {
-    const [schedules, setSchedules] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [alert, setAlert] = useState();
+  const [schedules, setSchedules] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [alert, setAlert] = useState();
 
-    useEffect(() => {
-        setLoading(true);
-        getSchedules();
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    getSchedules();
+  }, []);
 
-    const getSchedules = async () => {
+  const getSchedules = async () => {
+    var res = await services.GetSchedules();
 
-        var res = await services.GetSchedules();
-
-        if (res !== undefined && res.status === 200) {
-            res.json()
-                .then((response) => {
-                    setSchedules(response);
-                    setLoading(false);
-                })
-        }
-        else {
-            setAlert({
-                success: false,
-                message: "Greška pri čitanju iz baze"
-            });
-            setLoading(false);
-        }
+    if (res !== undefined && res.status === 200) {
+      res.json().then((response) => {
+        setSchedules(response);
+        setLoading(false);
+      });
+    } else {
+      setAlert({
+        success: false,
+        message: "Greška pri čitanju iz baze",
+      });
+      setLoading(false);
     }
-    return (
-        <>
-            {
-                alert !== undefined && alert !== null
-                    ? <Notification
-                        success={alert.success}
-                        message={alert.message}
-                        setAlert={setAlert}
-                    />
-                    : null
-            }
-            {
-                loading
-                    ? <Loading />
-                    : <div className='AllSchedules'>
-                        <h1>Svi izveštaji</h1>
-                        <table className='AllSchedulesTable'>
-                            <thead>
-                                <tr>
-                                    <th>Rb</th>
-                                    <th>Naziv</th>
-                                    <th>Datum generisanja</th>
-                                    <th>Procenat ispunjenosti zahteva</th>
-                                    <th>Odabran</th>
-                                    <th>Link</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    schedules.map((schedule, index) => <tr key={index}>
-                                        <td>{`${index + 1}.`}</td>
-                                        <td>{schedule.Name}</td>
-                                        <td>{schedule.GeneratedOn}</td>
-                                        <td>{schedule.Percentage}</td>
-                                        <td>{schedule.Chosen.data[0] === 1 ? <CheckCircleIcon sx={{ color: "#00FF00" }} /> : <CancelRoundedIcon sx={{ color: "red" }} />}</td>
-                                        <td>
-                                            <Link to={`/rasporedi/${schedule.ScheduleID}`}>
-                                                <ArrowForwardIosIcon />
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                    )
-                                }
-                            </tbody>
-                        </table>
-                    </div >
-            }
-        </>
-    )
+  };
+  return (
+    <>
+      {alert !== undefined && alert !== null ? (
+        <Notification
+          success={alert.success}
+          message={alert.message}
+          setAlert={setAlert}
+        />
+      ) : null}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="AllSchedules">
+          <h1>Svi izveštaji</h1>
+          <table className="AllSchedulesTable">
+            <thead>
+              <tr>
+                <th>Rb</th>
+                <th>Naziv</th>
+                <th>Datum generisanja</th>
+                <th>Procenat ispunjenosti zahteva</th>
+                <th>Odabran</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {schedules.map((schedule, index) => (
+                <tr key={index}>
+                  <td>{`${index + 1}.`}</td>
+                  <td>{schedule.Name}</td>
+                  <td>
+                    {new Date(schedule.GeneratedOn).toLocaleDateString("sr-RS")}
+                  </td>
+                  <td>{schedule.Percentage}</td>
+                  <td>
+                    {schedule.Chosen === 1 ? (
+                      <CheckCircleIcon sx={{ color: "#00FF00" }} />
+                    ) : (
+                      <CancelRoundedIcon sx={{ color: "red" }} />
+                    )}
+                  </td>
+                  <td>
+                    <Link to={`/rasporedi/${schedule.ScheduleID}`}>
+                      <ArrowForwardIosIcon />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
 }
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,184 +1,201 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useLocation } from 'react-router';
-import '../assets/styles/Schedule.css'
-import services from '../services/services';
-import Loading from './Loading';
-import Notification from './Notification';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router";
+import "../assets/styles/Schedule.css";
+import services from "../services/services";
+import Loading from "./Loading";
+import Notification from "./Notification";
 
 export default function Schedule({ setScheduleName }) {
-    const [schedule, setSchedule] = useState();
-    const [loading, setLoading] = useState(true);
-    const [alert, setAlert] = useState();
-    const weekdays = ["Ne","Po","Ut","Sr","Če","Pe","Su"];
+  const [schedule, setSchedule] = useState();
+  const [loading, setLoading] = useState(true);
+  const [alert, setAlert] = useState();
+  const weekdays = ["Ne", "Po", "Ut", "Sr", "Če", "Pe", "Su"];
 
-    const location = useLocation().pathname;
-    var loc = location.slice(location.lastIndexOf('/') + 1, location.length);
+  const location = useLocation().pathname;
+  var loc = location.slice(location.lastIndexOf("/") + 1, location.length);
 
-    useEffect(() => {
-        setLoading(true);
-        getSchedule();
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    getSchedule();
+  }, []);
 
-    const getSchedule = async () => {
-        var res = await services.GetSchedule(parseInt(loc));
+  const getSchedule = async () => {
+    var res = await services.GetSchedule(parseInt(loc));
 
-        if (res !== undefined && res.status === 200) {
-            res.json()
-                .then((response) => {
-                    setSchedule(response);
-                    setLoading(false);
-                    setScheduleName(schedule.Name);
-                })
-        }
-        else {
-            setAlert({
-                success: false,
-                message: "Greška pri dovlačenju podataka iz baze"
-            });
-            setLoading(false);
-        }
+    if (res !== undefined && res.status === 200) {
+      res.json().then((response) => {
+        setSchedule(response);
+        setLoading(false);
+        setScheduleName(schedule.Name);
+      });
+    } else {
+      setAlert({
+        success: false,
+        message: "Greška pri dovlačenju podataka iz baze",
+      });
+      setLoading(false);
+    }
+  };
+
+  const generateTableHeader = () => {
+    const headerNumbers = [];
+    const headerDays = [];
+    for (let i = 1; i <= schedule.NumberOfDays; i++) {
+      headerDays.push(
+        <th key={i}>
+          {weekdays[new Date(schedule.Year, schedule.Month - 1, i).getDay()]}
+        </th>
+      );
+      headerNumbers.push(<th key={i}>{i}</th>);
     }
 
-    const generateTableHeader = () => {
-        const headerNumbers = []
-        const headerDays = [];
-        for (let i = 1; i <= schedule.NumberOfDays; i++) {
-            headerDays.push(<th key={i}>{weekdays[new Date(schedule.Year, schedule.Month-1, i).getDay()]}</th>)
-            headerNumbers.push(<th key={i}>{i}</th>)
-        }
-
-        return (<>
-            <tr>
-            <th rowSpan={2}>Sestra/Tehničar</th>
-            {headerDays}
-            <th colSpan={3}>Br. sati</th>
-            </tr>
-            <tr>
-            {headerNumbers}
-            <th>R</th>
-            <th>O</th>
-            <th>Uk.</th>
-            </tr>
-        </>
-
-        );
-    }
-    const generateRows = () => {
-        const rows = [];
-
-        schedule.NursesAndDays.forEach((nurDay) => {
-            var row = [];
-            row.push(<th key={nurDay.NurseID}>{nurDay.NurseName}</th>)
-            let j = 1;
-
-            for (let i = 0; i < nurDay.Days.length; i++) {
-                if (nurDay.Days[i].Day === j) {
-                    row.push(<td style={{ 'text-align': 'center' }}>{nurDay.Days[i].Symbol}</td>)
-                    j++;
-                    if (i === nurDay.Days.length - 1 && j <= schedule.NumberOfDays) {
-                        for (let k = j; k <= schedule.NumberOfDays; k++) {
-                            row.push(<td></td>)
-                        }
-                    }
-                }
-                else {
-                    let k = i === 0 ? 1 : nurDay.Days[i - 1].Day + 1;
-                    if (i + 1 >= nurDay.Days.length) {
-                        for (k; k <= schedule.NumberOfDays; k++) {
-                            if (k === nurDay.Days[i].Day) {
-                                row.push(<td style={{ 'text-align': 'center' }}>{nurDay.Days[i].Symbol}</td>)
-                            }
-                            else
-                                row.push(<td></td>)
-                        }
-                    } else {
-                        for (k; k < nurDay.Days[i + 1].Day; k++) {
-                            if (k === nurDay.Days[i].Day) {
-                                row.push(<td style={{ 'text-align': 'center' }}>{nurDay.Days[i].Symbol}</td>)
-                            }
-                            else
-                                row.push(<td></td>)
-                        }
-                        j = k;
-                    }
-                }
-            }
-            row.push(<td style={{fontWeight: 'bold'}}>{calculateNumberOfHours(nurDay.Days)[0]}</td>)
-            row.push(<td style={{fontWeight: 'bold'}}>{calculateNumberOfHours(nurDay.Days)[1]}</td>)
-            row.push(<td style={{fontWeight: 'bold'}}>{calculateNumberOfHours(nurDay.Days)[2]}</td>)
-            rows.push(<tr>{row}</tr>)
-        })
-        return rows;
-    }
-    const calculateNumberOfHours = (days) => {
-        var hoursWork = 0;
-        var hoursVac = 0;
-        days.forEach((day) => {
-            if(day.Working) hoursWork += day.Duration;
-            else hoursVac += day.Duration;
-        })
-        return [hoursWork, hoursVac, hoursWork+hoursVac];
-    }
-    const choseThisSchedule = async () => {
-        var res = await services.ChoseSchedule(schedule.ScheduleID);
-
-        if (res !== undefined && res.status === 200) {
-            var s = schedule;
-            s.Chosen.data = [1]
-            setSchedule(s);
-            setAlert({
-                success: true,
-                message: "Uspešno odabran"
-            })
-        } else {
-            setAlert({
-                success: false,
-                message: "Greška pri odabiru"
-            })
-        }
-    }
     return (
-        <>
-            {
-                alert !== undefined && alert !== null
-                    ? <Notification
-                        success={alert.success}
-                        message={alert.message}
-                        setAlert={setAlert}
-                    />
-                    : null
+      <>
+        <tr>
+          <th rowSpan={2}>Sestra/Tehničar</th>
+          {headerDays}
+          <th colSpan={3}>Br. sati</th>
+        </tr>
+        <tr>
+          {headerNumbers}
+          <th>R</th>
+          <th>O</th>
+          <th>Uk.</th>
+        </tr>
+      </>
+    );
+  };
+  const generateRows = () => {
+    const rows = [];
+
+    schedule.NursesAndDays.forEach((nurDay) => {
+      var row = [];
+      row.push(<th key={nurDay.NurseID}>{nurDay.NurseName}</th>);
+      let j = 1;
+
+      for (let i = 0; i < nurDay.Days.length; i++) {
+        if (nurDay.Days[i].Day === j) {
+          row.push(
+            <td style={{ "text-align": "center" }}>{nurDay.Days[i].Symbol}</td>
+          );
+          j++;
+          if (i === nurDay.Days.length - 1 && j <= schedule.NumberOfDays) {
+            for (let k = j; k <= schedule.NumberOfDays; k++) {
+              row.push(<td></td>);
             }
-            {
-                loading
-                    ? <Loading />
-                    : <div className='Schedule'>
-                        <div className='schedule-header'>
-                            <h1>{schedule.Name}</h1>
-                            <button className='MyButton'
-                                disabled={schedule.Chosen.data[0] === 1}
-                                onClick={e => choseThisSchedule()}>Odaberi</button>
-                        </div>
-                        <table className='ScheduleTable'>
-                            <thead>
-                                    {generateTableHeader()}
-                            </thead>
-                            <tbody>
-                                {generateRows()}
-                            </tbody>
-                        </table>
-                        <div className='Signature'>
-                            <div className='alignLeft'>
-                                <p>{new Date(Date.now()).toLocaleDateString('sr-rs')}</p>
-                                <p>Beograd</p>
-                            </div>
-                            <div className='alignRight'>
-                                <p>_______________________</p>
-                                <p style={{ 'text-align': 'center' }}>Ružica Nikolić</p>
-                            </div>
-                        </div>
-                    </div>
+          }
+        } else {
+          let k = i === 0 ? 1 : nurDay.Days[i - 1].Day + 1;
+          if (i + 1 >= nurDay.Days.length) {
+            for (k; k <= schedule.NumberOfDays; k++) {
+              if (k === nurDay.Days[i].Day) {
+                row.push(
+                  <td style={{ "text-align": "center" }}>
+                    {nurDay.Days[i].Symbol}
+                  </td>
+                );
+              } else row.push(<td></td>);
             }
-        </>
-    )
+          } else {
+            for (k; k < nurDay.Days[i + 1].Day; k++) {
+              if (k === nurDay.Days[i].Day) {
+                row.push(
+                  <td style={{ "text-align": "center" }}>
+                    {nurDay.Days[i].Symbol}
+                  </td>
+                );
+              } else row.push(<td></td>);
+            }
+            j = k;
+          }
+        }
+      }
+      row.push(
+        <td style={{ fontWeight: "bold" }}>
+          {calculateNumberOfHours(nurDay.Days)[0]}
+        </td>
+      );
+      row.push(
+        <td style={{ fontWeight: "bold" }}>
+          {calculateNumberOfHours(nurDay.Days)[1]}
+        </td>
+      );
+      row.push(
+        <td style={{ fontWeight: "bold" }}>
+          {calculateNumberOfHours(nurDay.Days)[2]}
+        </td>
+      );
+      rows.push(<tr>{row}</tr>);
+    });
+    return rows;
+  };
+  const calculateNumberOfHours = (days) => {
+    var hoursWork = 0;
+    var hoursVac = 0;
+    days.forEach((day) => {
+      if (day.Working) hoursWork += day.Duration;
+      else hoursVac += day.Duration;
+    });
+    return [hoursWork, hoursVac, hoursWork + hoursVac];
+  };
+  const choseThisSchedule = async () => {
+    var res = await services.ChoseSchedule(schedule.ScheduleID);
+
+    if (res !== undefined && res.status === 200) {
+      var s = schedule;
+      s.Chosen = 1;
+      setSchedule(s);
+      setAlert({
+        success: true,
+        message: "Uspešno odabran",
+      });
+    } else {
+      setAlert({
+        success: false,
+        message: "Greška pri odabiru",
+      });
+    }
+  };
+  return (
+    <>
+      {alert !== undefined && alert !== null ? (
+        <Notification
+          success={alert.success}
+          message={alert.message}
+          setAlert={setAlert}
+        />
+      ) : null}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="Schedule">
+          <div className="schedule-header">
+            <h1>{schedule.Name}</h1>
+            <button
+              className="MyButton"
+              disabled={schedule.Chosen === 1}
+              onClick={(e) => choseThisSchedule()}
+            >
+              Odaberi
+            </button>
+          </div>
+          <table className="ScheduleTable">
+            <thead>{generateTableHeader()}</thead>
+            <tbody>{generateRows()}</tbody>
+          </table>
+          <div className="Signature">
+            <div className="alignLeft">
+              <p>{new Date(Date.now()).toLocaleDateString("sr-rs")}</p>
+              <p>Beograd</p>
+            </div>
+            <div className="alignRight">
+              <p>_______________________</p>
+              <p style={{ "text-align": "center" }}>Ružica Nikolić</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
