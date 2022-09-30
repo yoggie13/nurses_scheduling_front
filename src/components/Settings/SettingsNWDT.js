8273227be5aa4bbb0c2de108dcab6a8da6747af6
Ognjen <ognjennikolic13@gmail.com>
2022-09-30 15:21:15 +0200

    added addnewday

--- src/components/Settings/SettingsNWDT.js
+++ src/components/Settings/SettingsNWDT.js
@@ -1,173 +1,257 @@
-import React from 'react'
-import services from '../../services/services';
-import { useState, useEffect } from 'react';
-import { TextField } from '@mui/material';
-import Loading from '../Loading';
-import Notification from '../Notification';
-import { DeleteForeverOutlined } from '@mui/icons-material';
+import React from "react";
+import services from "../../services/services";
+import { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
+import Loading from "../Loading";
+import Notification from "../Notification";
+import { DeleteForeverOutlined } from "@mui/icons-material";
+import Modal from "../Modal";
 
 export default function SettingsNWDT() {
-    const [loading, setLoading] = useState(false);
-    const [alert, setAlert] = useState();
-    const [types, setTypes] = useState([]);
-    const [typesToChange, setTypesToChange] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState();
+  const [types, setTypes] = useState([]);
+  const [typesToChange, setTypesToChange] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [newDay, setNewDay] = useState({
+    Name: "",
+    Symbol: "",
+    NumberOfHours: 0,
+  });
 
-    useEffect(() => {
-        setLoading(true);
-        getTypes();
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    getTypes();
+  }, []);
 
-    const getTypes = async () => {
-        var res = await services.GetNWDTypes();
+  const isValid = (field) => {
+    if (field === undefined || field === null) return false;
+    return true;
+  };
 
-        if (res !== undefined && res.status === 200) {
-            res.json()
-                .then((result) => {
-                    setTypes(result);
-                    setLoading(false);
-                })
-        }
-        else {
-            setAlert({
-                success: false,
-                message: "Greška pri dovlačenju podataka"
-            });
-        }
+  const getTypes = async () => {
+    var res = await services.GetNWDTypes();
+
+    if (res !== undefined && res.status === 200) {
+      res.json().then((result) => {
+        setTypes(result);
+        setLoading(false);
+      });
+    } else {
+      setAlert({
+        success: false,
+        message: "Greška pri dovlačenju podataka",
+      });
     }
-    const handleNameChange = (e, index) => {
-        var t = types;
-        t[index].Name = e.target.value;
+  };
+  const handleNameChange = (e, index) => {
+    var t = types;
+    t[index].Name = e.target.value;
 
-        setTypes([...t]);
+    setTypes([...t]);
 
-        addTypesToChange(index);
+    addTypesToChange(index);
+  };
+  const handleSymbolChange = (e, index) => {
+    var t = types;
+    t[index].Symbol = e.target.value;
+
+    setTypes([...t]);
+
+    addTypesToChange(index);
+  };
+  const addTypesToChange = (index) => {
+    var n = typesToChange;
+    for (let i = 0; i < n.length; i++) {
+      if (n[i] === index) {
+        return;
+      }
     }
-    const handleSymbolChange = (e, index) => {
-        var t = types;
-        t[index].Symbol = e.target.value;
+    n.push(index);
+    setTypesToChange([...n]);
+  };
 
-        setTypes([...t]);
+  const handleSave = async (e) => {
+    setLoading(true);
 
-        addTypesToChange(index);
+    if (
+      typesToChange !== undefined &&
+      typesToChange !== null &&
+      typesToChange.length > 0
+    ) {
+      var editData = [];
+      for (let i = 0; i < typesToChange.length; i++) {
+        editData.push(types[typesToChange[i]]);
+      }
+
+      var res = await services.EditNWDTypes(editData);
+      if (res !== undefined && res.status === 200) {
+        setAlert({
+          success: true,
+          message: "Uspešno sačuvane izmene",
+        });
+        setLoading(false);
+      } else {
+        setAlert({
+          success: false,
+          message: "Greška pri čuvanju izmena",
+        });
+      }
     }
-    const addTypesToChange = (index) => {
-        var n = typesToChange;
-        for (let i = 0; i < n.length; i++) {
-            if (n[i] === index) {
-                return;
-            }
-        }
-        n.push(index)
-        setTypesToChange([...n]);
+  };
+  const handleDelete = async (id) => {
+    setLoading(true);
+    var res = await services.DeleteNWDTypes(id);
+
+    if (res !== undefined && res.status === 200) {
+      setAlert({
+        success: true,
+        message: "Uspešno obrisano",
+      });
+      getTypes();
+      setLoading(false);
+    } else {
+      setAlert({
+        success: false,
+        message: "Greška pri brisanju",
+      });
+      setLoading(false);
     }
-
-    const handleSave = async (e) => {
-        setLoading(true);
-
-        if (typesToChange !== undefined &&
-            typesToChange !== null &&
-            typesToChange.length > 0) {
-
-            var editData = [];
-            for (let i = 0; i < typesToChange.length; i++) {
-                editData.push(types[typesToChange[i]])
-            }
-
-            var res = await services.EditNWDTypes(editData);
-            if (res !== undefined && res.status === 200) {
-                setAlert({
-                    success: true,
-                    message: "Uspešno sačuvane izmene"
-                });
-                setLoading(false);
-            }
-            else {
-                setAlert({
-                    success: false,
-                    message: "Greška pri čuvanju izmena"
-                })
-            }
-        }
-    }
-    const handleDelete = async (id) => {
-        setLoading(true);
-        var res = await services.DeleteNWDTypes(id);
-
-        if (res !== undefined && res.status === 200) {
-            setAlert({
-                success: true,
-                message: "Uspešno obrisano"
-            });
-            getTypes();
-            setLoading(false);
-        }
-        else {
-            setAlert({
-                success: false,
-                message: "Greška pri brisanju"
-            });
-            setLoading(false);
-        }
-    }
-    const handleNumberChange = (e, index) => {
-
-        if (e.target.value !== "" && (isNaN(parseFloat(e.target.value)) || /[a-zA-Z]/g.test(e.target.value)))
-            return;
-        // if (e.target.value !== "" && !/^[0-9]+$/.test(e.target.value))
-        //     return;
-
-        var t = types;
-        t[index].NumberOfHours = e.target.value;
-
-        setTypes([...t]);
-
-        addTypesToChange(index);
-    }
-    return (
-        <>
-            {
-                loading
-                    ? <Loading />
-                    : <div className='SettingsArray'>
-                     {  types.map((type, index) => <div key={index} className='SettingsRow'>
-                            <TextField
-                                value={type.Name}
-                                className="TypeName"
-                                onChange={e => handleNameChange(e, index)}
-                                label="Naziv"
-                            />
-                            <TextField
-                                value={type.Symbol}
-                                className="TypeSymbol"
-                                onChange={e => handleSymbolChange(e, index)}
-                                label="Simbol"
-                            />
-                            <TextField
-                                value={type.NumberOfHours}
-                                className="TypeSymbol"
-                                onChange={e => handleNumberChange(e, index)}
-                                label="Br. sati"
-                            />
-                            <DeleteForeverOutlined
-                                onClick={e => handleDelete(type.NonWorkingDayTypeID)}
-                            />
-                        </div>)
-                        }
-                        <button className='MyButton' disabled={
-                            typesToChange === undefined || typesToChange === null || typesToChange.length <= 0
-                        }
-                        onClick={(e) => handleSave(e)}>Sačuvaj izmene</button>
-                    </div>
-            }
-            {
-                alert !== undefined && alert !== null
-                    ? <Notification
-                        success={alert.success}
-                        message={alert.message}
-                        setAlert={setAlert}
-                    />
-                    : null
-            }
-        </>
+  };
+  const handleNumberChange = (e, index) => {
+    if (
+      e.target.value !== "" &&
+      (isNaN(parseFloat(e.target.value)) || /[a-zA-Z]/g.test(e.target.value))
     )
+      return;
+    // if (e.target.value !== "" && !/^[0-9]+$/.test(e.target.value))
+    //     return;
+
+    var t = types;
+    t[index].NumberOfHours = e.target.value;
+
+    setTypes([...t]);
+
+    addTypesToChange(index);
+  };
+  const handleNewNumberChange = (e, index) => {
+    if (
+      e.target.value !== "" &&
+      (isNaN(parseFloat(e.target.value)) || /[a-zA-Z]/g.test(e.target.value))
+    )
+      return;
+    // if (e.target.value !== "" && !/^[0-9]+$/.test(e.target.value))
+    //     return;
+
+    setNewDay({ ...newDay, NumberOfHours: e.target.value });
+  };
+  const handleNewDaySave = async () => {
+    setLoading(true);
+    console.log(newDay.length);
+    if (
+      newDay !== undefined &&
+      newDay !== null &&
+      isValid(newDay.Name) &&
+      isValid(newDay.Symbol) &&
+      isValid(newDay.NumberOfHours)
+    ) {
+      var res = await services.AddNewDay(newDay);
+
+      if (res !== undefined && res.status === 200) {
+        setAlert({ success: true, message: "Uspešno dodavanje" });
+        getTypes();
+        setModal(false);
+      } else {
+        setAlert({ success: false, message: "Greška pri dodavanju" });
+        setLoading(false);
+      }
+    }
+  };
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="SettingsArray">
+          {types.map((type, index) => (
+            <div key={index} className="SettingsRow">
+              <TextField
+                value={type.Name}
+                className="TypeName"
+                onChange={(e) => handleNameChange(e, index)}
+                label="Naziv"
+              />
+              <TextField
+                value={type.Symbol}
+                className="TypeSymbol"
+                onChange={(e) => handleSymbolChange(e, index)}
+                label="Simbol"
+              />
+              <TextField
+                value={type.NumberOfHours}
+                className="TypeSymbol"
+                onChange={(e) => handleNumberChange(e, index)}
+                label="Br. sati"
+              />
+              <DeleteForeverOutlined
+                onClick={(e) => handleDelete(type.NonWorkingDayTypeID)}
+              />
+            </div>
+          ))}
+          <div>
+            <button className="MyButton" onClick={(e) => setModal(true)}>
+              Dodaj slobodan dan
+            </button>
+            <button
+              className="MyButton"
+              disabled={
+                typesToChange === undefined ||
+                typesToChange === null ||
+                typesToChange.length <= 0
+              }
+              onClick={(e) => handleSave(e)}
+            >
+              Sačuvaj izmene
+            </button>
+          </div>
+        </div>
+      )}
+      {alert !== undefined && alert !== null ? (
+        <Notification
+          success={alert.success}
+          message={alert.message}
+          setAlert={setAlert}
+        />
+      ) : null}
+      {modal ? (
+        <Modal
+          content={
+            <>
+              <TextField
+                label="Naziv"
+                value={newDay.Name}
+                onChange={(e) => setNewDay({ ...newDay, Name: e.target.value })}
+              />
+              <TextField
+                label="Simbol"
+                value={newDay.Symbol}
+                onChange={(e) =>
+                  setNewDay({ ...newDay, Symbol: e.target.value })
+                }
+              />
+              <TextField
+                label="Trajanje"
+                value={newDay.NumberOfHours}
+                onChange={(e) => handleNewNumberChange(e)}
+              />
+              <button className="MyButton" onClick={(e) => handleNewDaySave()}>
+                Dodaj
+              </button>
+            </>
+          }
+          setModal={setModal}
+        />
+      ) : null}
+    </>
+  );
 }
--- src/services/services.js
+++ src/services/services.js
@@ -1,88 +1,93 @@
 import api from "./api";
 
 export default class services {
-    static GetNursesForSelect = async () => {
-        return await api.GET("nursesForSelect/");
-    }
-    static GetDaysForSelect = async () => {
-        return await api.GET("daysForSelect/");
-    }
-    static TryNewSchedule = async (name, data) => {
-        return await api.POST("requests/", data);
-    }
-    static GetNurses = async () => {
-        return await api.GET("nurses/");
-    }
-    static DeleteNurses = async (data) => {
-        return await api.PUT("nurses/delete", data);
-    }
-    static EditNurses = async (data) => {
-        return await api.PUT("nurses/", data);
-    }
-    static AddNurse = async (data) => {
-        return await api.POST('nurses/', [data]);
-    }
-    static GetParameters = async () => {
-        return await api.GET('parameters/');
-    }
-    static EditParameters = async (data) => {
-        return await api.PUT('parameters/', data);
-    }
-    static GetShifts = async (data) => {
-        return await api.GET('shifts/', data);
-    }
-    static EditShifts = async (data) => {
-        return await api.PUT('shifts/', data);
-    }
-    static GetSequenceRules = async () => {
-        return await api.GET('sequencerules/');
-    }
-    static DeleteSequenceRule = async (id) => {
-        return await api.DELETE('sequencerules/' + id)
-    }
-    static GetGroupingRules = async () => {
-        return await api.GET('groupingrules/');
-    }
-    static GetNursesGroupingRules = async (id) => {
-        return await api.GET(`groupingrules/${id}/nurses`);
-    }
-    static DeleteNurseFromGroupingRule = async (GroupingRuleID, NurseID) => {
-        return await api.DELETE(`groupingrules/${GroupingRuleID}/nurses/${NurseID}`);
-    }
-    static AddNurseToGroupingRule = async (GroupingRuleID, NurseID) => {
-        return await api.POST(`groupingrules/${GroupingRuleID}/nurses/${NurseID}`);
-    }
-    static DeleteGroupingRule = async (id) => {
-        return await api.DELETE('groupingrules/' + id)
-    }
-    static EditGroupingRules = async (data) => {
-        return await api.PUT('groupingrules/', data)
-    }
-    static EditSequenceRules = async (data) => {
-        return await api.PUT('groupingrules/', data)
-    }
-    static GetPatterns = async () => {
-        return await api.GET('patterns/')
-    }
-    static EditPatterns = async (data) => {
-        return await api.PUT('patterns/', data);
-    }
-    static GetNWDTypes = async () => {
-        return await api.GET('nonworkingdaytypes/')
-    }
-    static EditNWDTypes = async(data) => {
-        return await api.POST('nonworkingdaytypes/', data)
-    }
-    static DeleteNWDTypes = async (id) => {
-        return await api.POST('nonworkingdaytypes/' + id)
-    }
-    static GetSchedules = async () => {
-        return await api.GET('schedules/');
-    }
-    static GetSchedule = async (id) => {
-        return await api.GET(`schedules/${id}`);
-    }
-    static ChoseSchedule = async (id) => {
-        return await api.POST(`schedules/${id}`);
-    }
+  static GetNursesForSelect = async () => {
+    return await api.GET("nursesForSelect/");
+  };
+  static GetDaysForSelect = async () => {
+    return await api.GET("daysForSelect/");
+  };
+  static TryNewSchedule = async (name, data) => {
+    return await api.POST("requests/", data);
+  };
+  static GetNurses = async () => {
+    return await api.GET("nurses/");
+  };
+  static DeleteNurses = async (data) => {
+    return await api.PUT("nurses/delete", data);
+  };
+  static EditNurses = async (data) => {
+    return await api.PUT("nurses/", data);
+  };
+  static AddNurse = async (data) => {
+    return await api.POST("nurses/", [data]);
+  };
+  static GetParameters = async () => {
+    return await api.GET("parameters/");
+  };
+  static EditParameters = async (data) => {
+    return await api.PUT("parameters/", data);
+  };
+  static GetShifts = async (data) => {
+    return await api.GET("shifts/", data);
+  };
+  static EditShifts = async (data) => {
+    return await api.PUT("shifts/", data);
+  };
+  static GetSequenceRules = async () => {
+    return await api.GET("sequencerules/");
+  };
+  static DeleteSequenceRule = async (id) => {
+    return await api.DELETE("sequencerules/" + id);
+  };
+  static GetGroupingRules = async () => {
+    return await api.GET("groupingrules/");
+  };
+  static GetNursesGroupingRules = async (id) => {
+    return await api.GET(`groupingrules/${id}/nurses`);
+  };
+  static DeleteNurseFromGroupingRule = async (GroupingRuleID, NurseID) => {
+    return await api.DELETE(
+      `groupingrules/${GroupingRuleID}/nurses/${NurseID}`
+    );
+  };
+  static AddNurseToGroupingRule = async (GroupingRuleID, NurseID) => {
+    return await api.POST(`groupingrules/${GroupingRuleID}/nurses/${NurseID}`);
+  };
+  static DeleteGroupingRule = async (id) => {
+    return await api.DELETE("groupingrules/" + id);
+  };
+  static EditGroupingRules = async (data) => {
+    return await api.PUT("groupingrules/", data);
+  };
+  static EditSequenceRules = async (data) => {
+    return await api.PUT("groupingrules/", data);
+  };
+  static GetPatterns = async () => {
+    return await api.GET("patterns/");
+  };
+  static EditPatterns = async (data) => {
+    return await api.PUT("patterns/", data);
+  };
+  static GetNWDTypes = async () => {
+    return await api.GET("nonworkingdaytypes/");
+  };
+  static EditNWDTypes = async (data) => {
+    return await api.POST("nonworkingdaytypes/", data);
+  };
+  static DeleteNWDTypes = async (id) => {
+    return await api.POST("nonworkingdaytypes/" + id);
+  };
+  static AddNewDay = async (data) => {
+    return await api.PUT("nonworkingdaytypes/", data);
+  };
+  static GetSchedules = async () => {
+    return await api.GET("schedules/");
+  };
+  static GetSchedule = async (id) => {
+    return await api.GET(`schedules/${id}`);
+  };
+  static ChoseSchedule = async (id) => {
+    return await api.POST(`schedules/${id}`);
+  };
 }
